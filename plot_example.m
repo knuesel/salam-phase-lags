8340d3306c4e9080ca63f14ce95be36d97e2b67c
Jeremie Knuesel <jeremie.knuesel@epfl.ch>
2016-09-13 09:23:23 +0200

    plot_example: fix smooth factor for random noise, warn in case of no lag data

--- plot_example.m
+++ plot_example.m
@@ -12,5 +12,14 @@ lags = (1:n) * 2*pi/(n+1);
 
 joint_angles = sin(2 * pi * repmat(t, 1, n) - repmat(lags, length(t), 1) + rand(length(t), n));
 
-out = phase_lags(t, joint_angles, 'Pattern', lags, 'splinesmooth', 0.00001);
-plot_phase_lags(out, []);
+out = phase_lags(t, joint_angles, 'Pattern', lags, 'splinesmooth', 0.99999);
+
+if isempty(out.lags)
+	warning('Could not get lags from data');
+	plot(t, out.joint_angles);
+else
+	disp('Lags:');
+	out.lags
+	%plot_phase_lags(out, []);
+	plot_phase_lags(out, [], 'PlotTargetPattern', false);
+end
