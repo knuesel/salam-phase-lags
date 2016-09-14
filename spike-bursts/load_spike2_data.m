e72ff41231dcad88d7f8a0efd0032206401d30e3
Jeremie Knuesel <jeremie.knuesel@epfl.ch>
2016-09-14 17:53:30 +0200

    load_spike2_data: better options, comments

--- spike-bursts/load_spike2_data.m
+++ spike-bursts/load_spike2_data.m
@@ -1,24 +1,50 @@
-function data = load_spike2_data(basename, side, roots)
+function data = load_spike2_data(basename, chan_type, chan_positions)
+% LOAD_SPIKE2_DATA    Load .mat files exported by Spike2
+%
+% LOAD_SPIKE2_DATA(BASENAME, CHAN_TYPE, CHAN_POSITIONS)
+%
+%    Loads the specified file, keeping the selected channels based on a
+%    heuristics on the channel names.
+%
+% INPUTS:
+%
+%    BASENAME:      name of the .mat file to import (without .mat extension)
+%
+%    CHAN_TYPE:     type of channels to keep for processing, one of:
+%                   'ipsi' (for ipsilateral channels)
+%                   'contra' (for contralateral channels)
+%                   'limbs' (for limb channels)
+%
+%    CHAN_POSITIONS: channel positions to keep (empty means keep all)
+%                    (the position is extracted from the channel name)
+%
+% OUTPUT: an array of channel structs, each with the following fields:
+%
+%   index:    the index of the channel in the Spike2 dataset
+%   positon:  the channel position extracted from its name (e.g. ventral root number)
+%   values:   the channel data
+%   interval: the sampling interval in seconds
+%   length:   the size of the data
 
     spike2 = load([basename '.mat']);
     spike2_fields = fieldnames(spike2);
     
     % Note: e.g. iVR_ and iVR\  must be listed before iVR
-    ipsi_roots = {'iVR_', 'iVR ', 'iVR', 'i_vr', 'VR_', 'VR ', 'VR', 'Sgt', 'h_vr', 'ipsiVR', 'ipsi VR', 'i_sg'};
-    contra_roots = {'cVR', 'c_vr', 'coVR ', 'CoVR', 'coVR', 'c_sg'};
-    limb_roots = {'SP_', 'SP ', 'sp_', 'sp ', 'SP', 'sp'};
+    ipsi_channels = {'iVR_', 'iVR ', 'iVR', 'i_vr', 'VR_', 'VR ', 'VR', 'Sgt', 'h_vr', 'ipsiVR', 'ipsi VR', 'i_sg'};
+    contra_channels = {'cVR', 'c_vr', 'coVR ', 'CoVR', 'coVR', 'c_sg'};
+    limb_channels = {'SP_', 'SP ', 'sp_', 'sp ', 'SP', 'sp'};
     
-    if strcmp(side, 'ipsi')
-        prefixes = ipsi_roots;
-    elseif strcmp(side, 'ipsi+limbs')
-        prefixes = [ipsi_roots limb_roots];
-    % elseif strcmp(side, 'ipsi+contra')
-    %     prefixes = [ipsi_roots contra_roots];
+    if strcmp(chan_type, 'ipsi')
+        prefixes = ipsi_channels;
+    elseif strcmp(chan_type, 'contra')
+        prefixes = contra_channels;
+    elseif strcmp(chan_type, 'limbs')
+        prefixes = limb_channels;
     else
-        prefixes = contra_roots;
+        error(['Invalid channel type ' chan_type])
     end
     
-    data = load_channels(spike2, prefixes, roots);
+    data = load_channels(spike2, prefixes, chan_positions);
 
 
 function channels = load_channels(spike2, prefixes, keep)
